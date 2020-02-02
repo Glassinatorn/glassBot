071f9cb69d13e6187fd104c6d2a085b1a6cff88b
Glassinatorn <gusten1995@gmail.com>
2020-02-02 17:34:21 +0100

    added cleaning functionality, but it needs improvements

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const { config } = require('dotenv');
-const { WebhookClient, Client, RichEmbed } = require('discord.js');
+const { TextChannel, WebhookClient, Client, RichEmbed } = require('discord.js');
 
 const client = new Client ({
     disableeveryone: true
@@ -13,8 +13,44 @@ client.on('ready', () => {
     console.log("glassBot logged in!");
 })
 
-clean = (channel) => {
-    console.log(channel.messages);
+clean = (msg) => {
+    const filter = m => m.content.includes('c');
+    const collector = msg.channel.createMessageCollector(filter, {time: 15000});
+    msg.channel.fetchMessages({limit: 100})
+        .then(messages => {
+            var tmp = messages.array();
+            var toDelete = [];
+            for(key in tmp) {
+                if (tmp[key].content === 'c' ||
+                    tmp[key].content === 'clean' ||
+                    tmp[key].content === 'pling' ||
+                    tmp[key].content === 'clean' ||
+                    tmp[key].content.includes('!play') ||
+                    tmp[key].content.includes('!clean') ||
+                    tmp[key].content.includes('!stop') ||
+                    tmp[key].content.includes('!resume') ||
+                    tmp[key].content.includes('!replay') ||
+                    tmp[key].content.includes('^play') ||
+                    tmp[key].content.includes('^clean') ||
+                    tmp[key].content.includes('^stop') ||
+                    tmp[key].content.includes('^resume') ||
+                    tmp[key].content.includes('^replay') ||
+                    tmp[key].author.username === 'glassbot' ||
+                    tmp[key].author.username === 'Yui')
+                {
+                    toDelete.push(tmp[key].id);
+                    console.log(tmp[key].content);
+                    console.log(tmp[key].author.username);
+                }
+            }
+            msg.channel.bulkDelete(toDelete, true)
+                .then(console.log('Deleted messages'))
+                .catch(console.error);
+        })
+        .catch(console.error);
+
+    // placeholder for what to send back
+    return collector.size;
 }
 
 client.on('message', msg => {
@@ -26,12 +62,12 @@ client.on('message', msg => {
             .setColor(0xFF0000)
             .setDescription('testing');
         msg.channel.send(embed);
-    } else if (msg.content === 'clean') {
-        hook.send('hook alive')
-            .then(msg => {
-                console.log('sent message through webhook');
-            })
-            .catch(console.error);
+    } else if (msg.content === 'c') {
+        if (msg.channel.type === 'text') {
+            clean(msg);
+        } else {
+            msg.reply('Not a channel I can clean');
+        }
     }
 });
 
